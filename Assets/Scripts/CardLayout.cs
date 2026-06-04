bf0feabab53a192350d30b21be04c049bc63fe23
rmoren15@ucsc.edu <rmoren15@ucsc.edu>
2026-06-04 04:26:30 -0700

    Finished UI For Game

--- Assets/Scripts/CardLayout.cs
+++ Assets/Scripts/CardLayout.cs
@@ -8,7 +8,11 @@ public class CardLayout : LayoutGroup
     public int rows;
     public int columns;
 
+    public Vector2 spacing;
+
     public Vector2 cardSize;
+
+    public int preferredTopPadding;
 
     public override void CalculateLayoutInputVertical()
     {
@@ -19,11 +23,13 @@ public class CardLayout : LayoutGroup
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
 
-        float cellHeight = parentHeight / rows;
+        float cellHeight = (parentHeight - 2 * preferredTopPadding - spacing.y * (rows - 1)) / rows;
         float cellWidth = cellHeight;
 
         cardSize = new Vector2(cellWidth, cellHeight);
-
+        padding.left = Mathf.FloorToInt((parentWidth - columns * cellWidth - spacing.x * (columns - 1)) / 2);
+        padding.top = Mathf.FloorToInt((parentHeight - rows * cellHeight - spacing.y * (rows - 1)) / 2);
+        padding.bottom = padding.top;
         for (int i = 0; i < rectChildren.Count; i++)
         {
             int rowCount = i / columns;
@@ -31,8 +37,8 @@ public class CardLayout : LayoutGroup
 
             var item = rectChildren[i];
 
-            var xPos = cardSize.x * columnCount;
-            var yPos = cardSize.y * rowCount;
+            var xPos = padding.left + cardSize.x * columnCount + spacing.x * (columnCount);
+            var yPos = padding.top + cardSize.y * rowCount + spacing.y * (rowCount);
 
             SetChildAlongAxis(item, 0, xPos, cardSize.x);
             SetChildAlongAxis(item, 1, yPos, cardSize.y);
